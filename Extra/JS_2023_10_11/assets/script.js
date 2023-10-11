7f9ef97c663b2ff57b25e92fc755157878fd7d06
KavinduAkash <bkkasamarasingha1220@gmail.com>
2023-10-11 20:05:55 +0530

    update js extra lecture - block scope variable declarations

--- Extra/JS_2023_10_11/assets/script.js
+++ Extra/JS_2023_10_11/assets/script.js
@@ -18,3 +18,29 @@ var isAllowed = true;
 var age = 28;
 var score = 90.8;
 
+console.log(typeof name);
+name = 10;
+
+let a = "hello";
+let b = 10;
+let c = true;
+
+b = 200;
+
+const z = 100;
+
+{
+    var school = "ijse";
+    let subject = "SE";
+    const max = 100;
+    console.log(school);
+    console.log(subject);
+    console.log(max);
+}
+
+console.log(school);
+console.log(subject);
+console.log(max);
+
+
+
