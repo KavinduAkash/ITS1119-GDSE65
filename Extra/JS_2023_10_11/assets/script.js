34d02cf11b05534d59fba3e6d1aa8baaabc82566
KavinduAkash <bkkasamarasingha1220@gmail.com>
2023-10-11 20:44:39 +0530

    update js extra lecture - js string methods part 1

--- Extra/JS_2023_10_11/assets/script.js
+++ Extra/JS_2023_10_11/assets/script.js
@@ -39,18 +39,37 @@ const z = 100;
 }
 
 console.log(school);
-console.log(subject);
-console.log(max);
+// console.log(subject);
+// console.log(max);
 
 // JS data types
 
-var x = 10; // number
-x = "Hello"; // string
-x = true; // boolean
-x = null; // object
-x = undefined; // undefined
-x = Symbol("foo"); // symbol
-x = BigInt("1231231231231"); // bigint
-x = {}; // object
-x = []; // object
-x = function () {}; // function
+// var x = 10; // number
+// x = "Hello"; // string
+// x = true; // boolean
+// x = null; // object
+// x = undefined; // undefined
+// x = Symbol("foo"); // symbol
+// x = BigInt("1231231231231"); // bigint
+// x = {}; // object
+// x = []; // object
+// x = function () {}; // function
+
+// String methods
+
+var myword = "hello";
+/*
+* h - 0
+* e - 1
+* l - 2
+* l - 3
+* o - 4
+* */
+
+console.log(myword.charAt(1)); // Returns the character at a specified index in a string.
+console.log(myword.charCodeAt(1)); // Returns the Unicode of the character at a specified index in a string.
+var x1 = "ijse";
+var x2 = "student";
+x1.concat(x2); // 'ijsestudent' - Combines two or more strings
+var x3 = "protal";
+x1.concat(x2, x3); // 'ijsestudentprotal'
