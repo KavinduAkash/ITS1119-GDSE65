f757898dfc016d3d5a7fb1a27810e0f345e8bd73
KavinduAkash <bkkasamarasingha1220@gmail.com>
2023-10-11 21:44:37 +0530

    update js extra lecture - js string methods part 2 and if condition

--- Extra/JS_2023_10_11/assets/script.js
+++ Extra/JS_2023_10_11/assets/script.js
@@ -67,9 +67,59 @@ var myword = "hello";
 * */
 
 console.log(myword.charAt(1)); // Returns the character at a specified index in a string.
+
 console.log(myword.charCodeAt(1)); // Returns the Unicode of the character at a specified index in a string.
+
 var x1 = "ijse";
 var x2 = "student";
 x1.concat(x2); // 'ijsestudent' - Combines two or more strings
 var x3 = "protal";
 x1.concat(x2, x3); // 'ijsestudentprotal'
+
+var x3 = "hello world!";
+x3.endsWith("d!"); // true - Checks whether a string ends with specified characters.
+
+String.fromCharCode(101); // 'e' - Converts Unicode values into characters.
+
+x3.includes("hello"); // true - Checks whether a string contains the specified substring.
+
+x3.indexOf("wo"); // 5 - Returns the index of the first occurrence of a specified value in a string.
+
+x3.lastIndexOf("wo"); // 11 - Returns the index of the last occurrence of a specified value in a string.
+
+'helloworld!world!'
+x3.split(""); // ['h', 'e', 'l', 'l', 'o', 'w', 'o', 'r', 'l', 'd', '!', 'w', 'o', 'r', 'l', 'd', '!']
+x3 = "hello,world,student";
+x3.split(","); // ['hello', 'world', 'student']
+
+var x4 = "HelloIjse";
+x4.toLowerCase(); // 'helloijse' Converts a string to lowercase.
+x4.toUpperCase(); // 'HELLOIJSE' Converts a string to uppercase.
+
+
+// if conditions in JS
+var max = 91;
+if(max > 90) {
+    console.log("Win");
+} else {
+    console.log("Sorry");
+}
+
+var mark = 70;
+
+// >= 75 -> A
+// >= 65 -> B
+// >= 50 -> C
+// < 50 -> F
+
+if(mark>=75) {
+    console.log("A");
+} else if(mark>=65) {
+    console.log("B");
+} else if(mark>=50) {
+    console.log("c");
+} else {
+    console.log("F");
+}
+
+
--- Extra/java_case/src/Main.java
+++ Extra/java_case/src/Main.java
@@ -3,9 +3,16 @@
  */
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello");
-        String name = "Kavindu";
-        Boolean isAllow = true;
+
+        int max = 3;
+
+        if(max > 5) {
+            System.out.println("Win");
+        } else if(max > 2) {
+            System.out.println("You are soo lucky");
+        } else {
+            System.out.println("Sorry Try again");
+        }
 
     }
 }
