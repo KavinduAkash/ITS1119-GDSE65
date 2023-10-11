8cd2c0f13255887cb8ae0a786dde7f4b30d27f32
KavinduAkash <bkkasamarasingha1220@gmail.com>
2023-10-11 19:33:11 +0530

    update extra js class - console

---
+++ Extra/java_case/src/Main.java
@@ -0,0 +1,15 @@
+/**
+ * @author hp
+ */
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello");
+
+        try {
+            // code
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+        }
+
+    }
+}
