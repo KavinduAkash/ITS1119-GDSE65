bc979258b4a42ba2195d9340e1ba11db3d9ad2b1
KavinduAkash <bkkasamarasingha1220@gmail.com>
2023-10-20 20:33:04 +0530

    update customer submit

--- Extra/POS/controller/CustomerController.js
+++ Extra/POS/controller/CustomerController.js
@@ -1,3 +1,5 @@
+const sriLankanMobileNumberRegex = /^(\+94|0)[1-9][0-9]{8}$/;
+
 $('#customer-btns>button').eq(0).on('click', () => {
 
     let customer_id = $('#customer_id').val();
@@ -5,13 +7,39 @@ $('#customer-btns>button').eq(0).on('click', () => {
     let customer_last_name = $('#customer_last_name').val();
     let customer_mobile = $('#customer_mobile').val();
 
-    let customer = {
-        customer_id: customer_id,
-        customer_first_name: customer_first_name,
-        customer_last_name: customer_last_name,
-        customer_mobile: customer_mobile
-    }
 
-    console.log(customer);
+    if(customer_id) {
+
+        if(customer_first_name) {
+
+            if(customer_last_name) {
+
+                let isValid = sriLankanMobileNumberRegex.test(customer_mobile);
+                if(customer_mobile && isValid) {
+
+                    let customer = {
+                        customer_id: customer_id,
+                        customer_first_name: customer_first_name,
+                        customer_last_name: customer_last_name,
+                        customer_mobile: customer_mobile
+                    };
+
+                    console.log(customer);
+
+                } else {
+                    alert("Invalid Customer Mobile Number");
+                }
+
+            } else {
+                alert("Invalid Customer Last Name");
+            }
+
+        } else {
+            alert("Invalid Customer First Name");
+        }
+
+    } else {
+        alert("Invalid Customer Id");
+    }
 
 });
